09d042374b1e58a67b0790815052f9111cce47c3
CHENZHIXIU99 <chenzhixiu99@qq.com>
2026-07-30 16:35:59 +0800

    Add installable PWA assets

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+const CACHE="back-to-self-v2";
+const scopeUrl=self.registration.scope;
+const core=["","manifest.webmanifest","icon.svg"].map(path=>new URL(path,scopeUrl).toString());
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(core))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match(scopeUrl))))});
